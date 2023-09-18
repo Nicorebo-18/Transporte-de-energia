9d8a056fdc107777f68a6b76798dcbc57ef25876
Nicorebo18 <nicorebo18@gmail.com>
2023-09-18 18:16:45 +0200

    Initial Commit

---
+++ Plantilla.m
@@ -0,0 +1,18 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               %
+%          ------------ PROBLEMA X-X --------------             %
+%                    Clase XX/XX/2023                           %
+%                  Nicolás Rebollo Ugarte                       %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc                       % Borrar toda la Consola / Ventana de comandos
+clear                     % Borra todas las variables
+format shortEng           % Pone el formato de ingeniería
+
+
+% ----------- Definir variables -----------
+
+% ----------- Cálculos-----------
+
+% ----------- Imprimir Resultados -----------
