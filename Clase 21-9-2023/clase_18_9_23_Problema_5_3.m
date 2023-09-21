b68d76f1a95275cd922be7eb9b3fb03792cb6151
Nicorebo18 <nicorebo18@gmail.com>
2023-09-21 15:42:11 +0200

    Problema 5.3 REVISAR

--- Clase 21-9-2023/clase_18_9_23_Problema_5_3.m
+++ Clase 21-9-2023/clase_18_9_23_Problema_5_3.m
@@ -22,6 +22,9 @@ Bu = 2.8e-9i;               % Susceptancia Unitaria
 
 
 % ----------- Cálculos-----------
+
+% Apartado A -----------
+
 Zu = Ru + Xu;
 Yu = Bu;
 gamma = sqrt(Zu*Yu);
@@ -36,6 +39,48 @@ U20 = U1/A;
 I10 = C*U20;
 modU20 = abs(U20);
 modI10 = abs(I10);
+
+% Apartado B ------------
+
+tol = 0.1;
+while abs(modU20 - 235e3) > tol
+    if modU20 > 235e3
+        long -= 0.1;
+    else
+        long += 0.1;
+    end
+
+    A = cosh(gamma*long);
+    U20 = U1/A;
+    modU20 = abs(U20) * sqrt(3);
+    printf("Módulo U20 = %d; Long = %d\n", modU20, long)
+end
+
+% Apartado C ------------
+
+% Como la frecuencia va a cambiar, hay que ajustar los parámetros
+%Lu = Xu / (2*pi*f);         % Cálculo de la Inductancia por unidad de longitud
+%Cu = Bu / (2*pi*f);         % Cálculo de la Capacidad por unidad de longitud
+%
+%tol = 1e-3;
+%while abs(modU20 - 250e3) > tol
+%    if modU20 > 250e3
+%        f -= 0.01;
+%    else
+%        f += 0.01;
+%    end
+%
+%    xu = 2i*pi*f*Lu;
+%    yu = 2i*pi*f*Cu;
+%    zu = Ru + xu;
+%
+%    Gamma = sqrt(zu*yu);
+%
+%    a = cosh(Gamma*long);
+%    U20 = U1/a;
+%    modU20 = abs(U20) * sqrt(3)
+%    f
+%end
 
 
 % ----------- Imprimir Resultados -----------
