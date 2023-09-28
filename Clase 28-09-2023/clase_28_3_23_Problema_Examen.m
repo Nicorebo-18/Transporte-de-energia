a3acea8081151122e848cc61ef5af6375eba4ed5
Nicorebo18 <nicorebo18@gmail.com>
2023-09-28 17:48:07 +0200

    Problema Examen Corregido

--- Clase 28-09-2023/clase_28_3_23_Problema_Examen.m
+++ Clase 28-09-2023/clase_28_3_23_Problema_Examen.m
@@ -15,13 +15,13 @@ format shortEng           % Pone el formato de ingeniería
 U2 = 220e3/sqrt(3);
 S = 100e6/3;
 fdp2 = 0.8;
-Ru = 71.2e-3/1e3;       % Ohm / m
+Ru = 72.1e-3/1e3;       % Ohm / m
 Xu = 1i*423.2e-3/1e3;   % Ohm / m
-Bu = 2693e-9/1e-3;      % S / m
+Bu = 2693e-9/1e3;       % S / m
 
 % ----------- Cálculos-----------
 P2 = S * fdp2;
-Q2 = S * tan(acos(fdp2));
+Q2 = P2 * tan(acos(fdp2));
 S2 = P2 + 1i*Q2;
 I2 = conj(S2/U2);
 
@@ -29,7 +29,7 @@ Zu = Ru + Xu;
 Yu = 1i*Bu;
 gamma = sqrt(Zu*Yu);
 Zc = sqrt(Zu/Yu);
-long = 81.817e3;
+long = 81.817e3;        % Long
 
 A = cosh(gamma*long);
 B = Zc * sinh(gamma*long);
@@ -38,7 +38,7 @@ D = A;
 
 UI1 = [A B; C D]*[U2; I2];
 abs(UI1(1)) * sqrt(3)   % Debe estar entre 209kV < U1 < 231kV
-delta = angle(UI1(1));  % 49.128 milirdianes < 30 grados (limite) => Estable
+delta = angle(UI1(1))  % 49.128 milirdianes < 30 grados (limite) => Estable
 
 % ----------- Imprimir Resultados -----------
 %disp("\nResultados: \n")
