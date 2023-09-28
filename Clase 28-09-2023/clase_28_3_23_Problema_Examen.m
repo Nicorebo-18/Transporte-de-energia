e6acef7659091143cc76dc3e46f2f8b43241cd91
Nicorebo18 <nicorebo18@gmail.com>
2023-09-28 16:34:04 +0200

    Problema examen

--- Clase 28-09-2023/clase_28_3_23_Problema_Examen.m
+++ Clase 28-09-2023/clase_28_3_23_Problema_Examen.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                               %
-%          ------------ PROBLEMA 5-11 --------------            %
-%                    Clase 28/09/2023                           %
+%          ------------ PROBLEMA X-X --------------             %
+%                    Clase XX/XX/2023                           %
 %                  Nicolás Rebollo Ugarte                       %
 %                                                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -12,43 +12,37 @@ format shortEng           % Pone el formato de ingeniería
 
 
 % ----------- Definir variables -----------
-A = 0.86;
-B = 130.2i;
-C = 0.002i;
-D = A;
-S2 = 1000e6/3;
-U2 = 500e3/sqrt(3);
-fdp2 = 0.8;     % En retraso
-
+U2 = 220e3/sqrt(3);
+S = 100e6/3;
+fdp2 = 0.8;
+Ru = 71.2e-3/1e3;       % Ohm / m
+Xu = 1i*423.2e-3/1e3;   % Ohm / m
+Bu = 2693e-9/1e-3;      % S / m
 
 % ----------- Cálculos-----------
+P2 = S * fdp2;
+Q2 = S * tan(acos(fdp2));
+S2 = P2 + 1i*Q2;
+I2 = conj(S2/U2);
 
-% Apartado A
-P2 = S2 * fdp2;
-Q2 = S2 * tan(acos(fdp2));
-S2total = P2 + 1i * Q2;
-I2 = conj(S2total/U2);
-UI1_a = [A B;C D] * [U2;I2];
+Zu = Ru + Xu;
+Yu = 1i*Bu;
+gamma = sqrt(Zu*Yu);
+Zc = sqrt(Zu/Yu);
+long = 81.817e3;
 
-% Apartado B
-Xc = -50i;
-Tc = [1 Xc; 0 1];
-TL = [A B; C D];
-UI1_b = Tc * TL * Tc * [U2;I2];
+A = cosh(gamma*long);
+B = Zc * sinh(gamma*long);
+C = sinh(gamma*long)/Zc;
+D = A;
 
+UI1 = [A B; C D]*[U2; I2];
+abs(UI1(1)) * sqrt(3)   % Debe estar entre 209kV < U1 < 231kV
+delta = angle(UI1(1));  % 49.128 milirdianes < 30 grados (limite) => Estable
 
 % ----------- Imprimir Resultados -----------
-disp("Resultados:")
+%disp("\nResultados: \n")
 
-disp("\nApartado A")
-%UI1_a
-abs(UI1_a(1))*sqrt(3)     % 622.15e3
-abs(UI1_a(2))             % 794.64
-
-disp("\nApartado B")
-%UI1_b
-abs(UI1_b(1))*sqrt(3)     % 530.75e3
-abs(UI1_b(2))             % 891.1416e
 
 % # Herramientas para imprimir en octave #
 
