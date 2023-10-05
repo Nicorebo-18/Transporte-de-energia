1c5a2bcfb4895961fd16f5598b3dc1b2f5520473
Nicorebo18 <nicorebo18@gmail.com>
2023-10-05 16:05:09 +0200

    Problema 2-11 a)

--- Clase 05-10-2023/clase_05_10_23_Problema_2_11.m
+++ Clase 05-10-2023/clase_05_10_23_Problema_2_11.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                               %
-%          ------------ PROBLEMA X-X --------------             %
-%                    Clase XX/XX/2023                           %
+%          ------------ PROBLEMA 2-11 --------------            %
+%                    Clase 05/10/2023                           %
 %                  Nicolás Rebollo Ugarte                       %
 %                                                               %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -13,7 +13,62 @@ format shortEng           % Pone el formato de ingeniería
 
 % ----------- Definir variables -----------
 
+% Datos Trafo 1
+ST1 = 80e6;
+UBTT1 = 12.2e3;
+UATT1 = 161e3;
+xT1 = 0.1i;
+
+% Datos Trafo 2
+ST2 = 40e6;
+UBTT2 = 13.8e3;
+UATT2 = 161e3;
+xT2 = 0.1i;
+
+% Definimos bases
+Sb = 100e6;
+Ub2 = 132e3;
+
+% Bases secundarias
+Ub1 = Ub2*(UBTT1/UATT1);
+xt1 = xT1*(Sb/ST1)*(UATT1/Ub2)^2
+Ub3 = Ub2*(UBTT2/UATT2);
+xt2 = xT2*(Sb/ST2)*(UATT2/Ub2)^2
+
+Zb2 = Ub2^2/Sb;
+Ib2 = Sb/(Ub2*sqrt(3));
+Zb3 = Ub3^2/Sb;
+Ib3 = Sb/(Ub3*sqrt(3));
+
 % ----------- Cálculos -----------
+
+% Paso parámetros a por unidad
+Z24 = 40 + 60i;
+Z23 = 20 + 80i;
+Z34 = 20 + 80i;
+
+z24 = Z24/Zb2
+z23 = Z23/Zb2
+z34 = Z34/Zb2
+
+SCARGA = 50e6;
+fdp = 0.8;   %  ╔═══ En Retraso (+) -> Si fuese en adelanto sería (-)
+U3 = 154e3;  %  V
+S3 = SCARGA*fdp + 1i*SCARGA*sin(acos(fdp));
+s3 = S3/Sb
+
+% Calculo de reactancia de los generadores
+SG1 = 50e6;
+UG1 = 12.2e3;
+xG1 = 0.15i;
+xg1 = xG1*(Sb/SG1)*(UG1/Ub1)^2
+
+SG2 = 20e6;
+UG2 = 13.8e3;
+xG2 = 0.15i;
+xg2 = xG2*(Sb/SG2)*(UG2/Ub3)^2
+
+
 
 % ----------- Imprimir Resultados -----------
 disp("\nResultados: \n")
