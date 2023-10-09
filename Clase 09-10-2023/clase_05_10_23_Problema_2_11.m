6dd01f2b29986bd06e3b09ab6f27dfa9e48f0ca5
Nicorebo18 <nicorebo18@gmail.com>
2023-10-09 15:16:27 +0200

    Problema 2-11 falta el u3 por definir

--- Clase 09-10-2023/clase_05_10_23_Problema_2_11.m
+++ Clase 09-10-2023/clase_05_10_23_Problema_2_11.m
@@ -31,9 +31,9 @@ Ub2 = 132e3;
 
 % Bases secundarias
 Ub1 = Ub2*(UBTT1/UATT1);
-xt1 = xT1*(Sb/ST1)*(UATT1/Ub2)^2
+xt1 = xT1*(Sb/ST1)*(UATT1/Ub2)^2;
 Ub3 = Ub2*(UBTT2/UATT2);
-xt2 = xT2*(Sb/ST2)*(UATT2/Ub2)^2
+xt2 = xT2*(Sb/ST2)*(UATT2/Ub2)^2;
 
 Zb2 = Ub2^2/Sb;
 Ib2 = Sb/(Ub2*sqrt(3));
@@ -47,31 +47,42 @@ Z24 = 40 + 60i;
 Z23 = 20 + 80i;
 Z34 = 20 + 80i;
 
-z24 = Z24/Zb2
-z23 = Z23/Zb2
-z34 = Z34/Zb2
+z24 = Z24/Zb2;
+z23 = Z23/Zb2;
+z34 = Z34/Zb2;
 
 SCARGA = 50e6;
 fdp = 0.8;   %  ╔═══ En Retraso (+) -> Si fuese en adelanto sería (-)
 U3 = 154e3;  %  V
 S3 = SCARGA*fdp + 1i*SCARGA*sin(acos(fdp));
-s3 = S3/Sb
+s3 = S3/Sb;
 
 % Calculo de reactancia de los generadores
 SG1 = 50e6;
 UG1 = 12.2e3;
 xG1 = 0.15i;
-xg1 = xG1*(Sb/SG1)*(UG1/Ub1)^2
+xg1 = xG1*(Sb/SG1)*(UG1/Ub1)^2;
 
 SG2 = 20e6;
 UG2 = 13.8e3;
 xG2 = 0.15i;
-xg2 = xG2*(Sb/SG2)*(UG2/Ub3)^2
+xg2 = xG2*(Sb/SG2)*(UG2/Ub3)^2;
 
+% Apartado a
+i3 = conj(s3/u3);
+zeq = xt1 + ((z24+z34)^-1 + z23^-1)^-1;
+u1 = u3 + i3*zeq;
+U1 = abs(u1)*Ub1/1e3;   % En kV
+
+% Apartado b
+zcarga = abs(u3)^2/conj(s3);
 
 
 % ----------- Imprimir Resultados -----------
 disp("\nResultados: \n")
+
+disp(["Tension en el nudo U1: " num2str(U1) " kV "]) %254.1173e3
+disp(["Impedancia por unidad de carga: " num2str(U1) " Ohm "]) %379.26+284.45i
 
 
 % # Herramientas para imprimir en octave #
