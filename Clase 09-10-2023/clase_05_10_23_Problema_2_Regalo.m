c997b06df7b68371826a36d302025055ce46161c
Nicorebo18 <nicorebo18@gmail.com>
2023-10-09 15:53:54 +0200

    Crear carpeta de problemas Terminados

---
+++ Clase 09-10-2023/clase_05_10_23_Problema_2_Regalo.m
@@ -0,0 +1,48 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               %
+%        ------------ PROBLEMA 2-Regalo --------------          %
+%                    Clase 09/10/2023                           %
+%                  Nicolás Rebollo Ugarte                       %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc                       % Borrar toda la Consola / Ventana de comandos
+clear                     % Borra todas las variables
+format shortEng           % Pone el formato de ingeniería
+
+
+% ----------- Definir variables -----------
+ST = 15e6;
+UBT = 13.8e3;
+UAT = 69e3;
+U1 = 69e3;
+ZL = 0.19138 + 0.12119i;
+Zcarga_Y = 34 + 91.125i;
+
+% Bases Principales
+Sb = ST;
+Ub1 = U1;
+Ub2 = ;
+
+% Bases Derivadas
+
+
+
+
+% ----------- Cálculos -----------
+
+
+% ----------- Imprimir Resultados -----------
+disp("\nResultados: \n")
+
+
+% # Herramientas para imprimir en octave #
+
+% printf("",) 
+% --- Para Plots ---
+% h=plot (x, sin (x));
+% xlabel ("x");
+% ylabel ("sin (x)");
+% title ("Simple 2-D Plot");
+% waitfor(h);
+% -------------------
