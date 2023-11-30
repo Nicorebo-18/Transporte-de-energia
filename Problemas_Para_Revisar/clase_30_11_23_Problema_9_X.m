73c24e1c6cc919bb548346075474a12ccbd1f6e6
Nicorebo18 <nicorebo18@gmail.com>
2023-11-30 16:38:13 +0100

    Inicio problema desconocido

---
+++ Problemas_Para_Revisar/clase_30_11_23_Problema_9_X.m
@@ -0,0 +1,41 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                               %
+%          ------------ PROBLEMA 9-X --------------             %
+%                    Clase 30/11/2023                           %
+%                  Nicolás Rebollo Ugarte                       %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clc                       % Borrar toda la Consola / Ventana de comandos
+clear                     % Borra todas las variables
+format shortEng           % Pone el formato de ingeniería
+
+
+% ----------- Definir variables -----------
+
+
+
+
+% ----------- Cálculos -----------
+
+
+
+
+% ----------- Imprimir Resultados -----------
+disp("\nResultados: \n")
+
+
+
+
+% # Herramientas para imprimir en octave #
+
+% printf("",) 
+% disp() con [num2str()]
+
+% --- Para Plots ---
+% h=plot (x, sin (x));
+% xlabel ("x");
+% ylabel ("sin (x)");
+% title ("Simple 2-D Plot");
+% waitfor(h);
+% -------------------
